0f046b0a14844beb6d5e1ee5adcfeeda0e267719
Neha Patel <nehajp999@gmail.com>
2020-09-16 19:06:44 +0100

    Percentage JS Programme

---
+++ Lecture-1/Lec_3/Percentage.js
@@ -0,0 +1,10 @@
+function percentage (p, c, m){
+    var addition = p + c + m;
+    var percent = addition * 100 / 300;
+    return percent
+}
+
+    var result = percentage(75,80,90);
+    var result1 = percentage(85, 90, 95);
+    console.log (result);
+    console.log (result1);
